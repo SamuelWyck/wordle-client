87475eef4e7d3742c9bec5064e02f5ebe94cd36b
Samuel <samawyck@gmail.com>
2026-01-13 11:17:15 -0600

    Hook up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './components/header';
+import { Outlet } from 'react-router-dom';
 
 
 
 function App() {
 	return (
 		<>	
+		<Header />
+		<Outlet />
+		<p></p>
 		</>
 	);
 };
