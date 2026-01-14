a31910ffed6608287c2c348dbd2581d60d1097a7
Samuel <samawyck@gmail.com>
2026-01-13 18:03:23 -0600

    Stub out useEffect to get wordle game guesses

--- src/components/wordle.jsx
+++ src/components/wordle.jsx
@@ -1,13 +1,36 @@
 import "../styles/wordle.css";
 import WordleInput from "./wordleInput.jsx";
+import {useState, useEffect} from "react";
+import apiManager from "../utils/apiManager.js";
 
 
 
-function Wordle() { 
+function Wordle() {
+    const [inputs, setInputs] = useState(buildInputs());
+
+
+    useEffect(function() {
+        apiManager.getWordleGuesses();
+    }, []);
+
+
+    function buildInputs() {
+        const inputs = [];
+        for (let i = 0; i < 6; i += 1) {
+            const input = <WordleInput 
+                active={false} maxLength={5} 
+                submitCb={function(val) {console.log(val)}} 
+                key={i}
+            />;
+            inputs.push(input);
+        }
+        return inputs;
+    };
+
     return (
-    <main>
-        <div className="board">
-            <WordleInput active={true} maxLength={5} submitCb={function(val) {console.log(val)}} />
+    <main className="wordle-main">
+        <div className="wordle-board">
+            {inputs}
         </div>
     </main>
     );
