7d29ce759fce082a5a9b739e60e9b8de7e0f9def
Samuel <samawyck@gmail.com>
2026-01-14 12:28:33 -0600

    Factor out game size

--- src/routes.jsx
+++ src/routes.jsx
@@ -11,7 +11,7 @@ const routes = [
         children: [
             {
                 index: true,
-                element: <Wordle />
+                element: <Wordle maxGuesses={6} wordLength={5}/>
             }
         ]
     }
