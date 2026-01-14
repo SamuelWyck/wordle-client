0c60d6bca64c2758e4de28021c13162f99aa8af6
Samuel <samawyck@gmail.com>
2026-01-14 12:28:19 -0600

    Add methods to makes guesses and handle worlde session storage

--- src/utils/apiManager.js
+++ src/utils/apiManager.js
@@ -11,9 +11,16 @@ class ApiManager {
     async #makeApiCall(url, options) {
         try {
             const res = await fetch(url, options);
+
+            const wordleSessionId = res.headers.get("set-wordle-session-id");
+            if (wordleSessionId) {
+                this.#storage.saveWordleSessionId(wordleSessionId);
+            }
+
             const jsonRes = await res.json();
             return jsonRes;
-        } catch {
+        } catch (error) {
+            console.log(error);
             return {errors: [{msg: "Unable to connect to server"}]};
         }
     };
@@ -21,23 +28,67 @@ class ApiManager {
     async getWordleGuesses() {
         const url = `${this.#apiDomain}/wordle/guesses`;
 
+        const headers = {};
+        const [wordleKey, wordleVal] = this.#getWordleHeader();
+        if (wordleKey !== null) {
+            headers[wordleKey] = wordleVal;
+        }
+
         const options = {
             method: "GET",
-            headers: this.#getWordleHeaders()
+            headers: headers,
+            mode: "cors"
         };
 
         const response = await this.#makeApiCall(url, options);
-        console.log(response);
         return response;
     };
 
-    #getWordleHeaders() {
-        const headers = {};
+    #getWordleHeader() {
         const sessionId = this.#storage.getWordleSessionId();
-        if (sessionId !== null) {
-            headers[this.#wordleSessionHeader] = sessionId;
+        let headerKey = this.#wordleSessionHeader;
+        if (sessionId === null) {
+            headerKey = null;
         }
-        return headers;
+        return [headerKey, sessionId];
+    };
+
+    async makeWordleGuess(word) {
+        const url = `${this.#apiDomain}/wordle/guess/${word}`;
+
+        const headers = {};
+        const [wordleKey, wordleVal] = this.#getWordleHeader();
+        if (wordleKey !== null) {
+            headers[wordleKey] = wordleVal;
+        }
+
+        const options = {
+            mode: "cors",
+            method: "GET",
+            headers: headers
+        };
+
+        const response = await this.#makeApiCall(url, options);
+        return response;
+    };
+
+    async getWordOfTheDay() {
+        const url = `${this.#apiDomain}/wordle/todays-word`;
+
+        const headers = {};
+        const [wordleKey, wordleVal] = this.#getWordleHeader();
+        if (wordleKey !== null) {
+            headers[wordleKey] = wordleVal;
+        }
+
+        const options = {
+            mode: "cors",
+            method: "GET",
+            headers: headers
+        };
+
+        const res = await this.#makeApiCall(url, options);
+        return res;
     };
 };
 
