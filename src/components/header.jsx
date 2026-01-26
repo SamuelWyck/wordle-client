179b4920ee4e2a451be155f657e7c736cbf8460d
Samuel <samawyck@gmail.com>
2026-01-26 14:42:42 -0600

    Add logic to save and fetch color mode setting

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,16 +2,41 @@ import "../styles/header.css";
 import logo from "../assets/crosswordLogo.png";
 import { Link } from "react-router-dom";
 import ToggleButton from "./toggleButton.jsx";
+import storageManager from "../utils/storageManager.js";
 
 
 
 function Header() {
+    const lightMode = "light";
+    const darkMode = "dark";
 
 
     function toggleLightMode() {
         const html = document.querySelector("html");
-        html.classList.toggle("light");
+        const wasDarkMode = html.classList.toggle("light");
+        const newMode = (wasDarkMode) ? lightMode : darkMode;
+        storageManager.saveColorModeSetting(newMode);
     };
+
+
+    function isDarkMode() {
+        const colorMode = storageManager.getColorModeSetting();
+        return colorMode === darkMode || colorMode === null;
+    };
+
+
+    function setInitialColorMode() {
+        const colorMode = storageManager.getColorModeSetting();
+        if (colorMode === darkMode) {
+            return;
+        }
+        
+        const html = document.querySelector("html");
+        html.classList.add("light");
+    };
+
+
+    setInitialColorMode();
 
 
     return (
@@ -28,7 +53,7 @@ function Header() {
             <nav>
                 <Link to={"/"}>Wordle</Link>
             </nav>
-            <ToggleButton startOn={true} clickCallback={toggleLightMode} />
+            <ToggleButton startOn={isDarkMode()} clickCallback={toggleLightMode} />
         </div>
     </header>
     );
