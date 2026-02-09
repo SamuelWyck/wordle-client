727233facfea042c41fbc22ded3c8496dda01101
Samuel <samawyck@gmail.com>
2026-02-09 14:46:33 -0600

    Hook up 2048 route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import "./App.jsx";
 import App from "./App.jsx";
 import Wordle from "./components/wordle.jsx";
 import SudokuElement from "./components/sudokuElement.jsx";
+import TwentyFourtyEightElement from "./components/TwentyFourtyEightElement.jsx";
 import { Navigate } from "react-router-dom";
 
 
@@ -16,12 +17,16 @@ const routes = [
                 element: <Wordle maxGuesses={6} wordLength={5}/>
             },
             {
-                path: "*",
-                element: <Navigate to={"/"} replace={true} />
-            },
-            {
                 path: "/sudoku",
                 element: <SudokuElement />
+            },
+            {
+                path: "/2048",
+                element: <TwentyFourtyEightElement />
+            },
+            {
+                path: "*",
+                element: <Navigate to={"/"} replace={true} />
             }
         ]
     }
