d62cbb68a70ec2cf354418d674d21fac66c01bdb
Samuel <samawyck@gmail.com>
2026-02-12 10:24:54 -0600

    Add projects info

---
+++ src/utils/projects.js
@@ -0,0 +1,28 @@
+import g2048Img from "../assets/projectImages/2048.png";
+import sudokuImg from "../assets/projectImages/sudoku.png";
+import wordleImg from "../assets/projectImages/wordle.png";
+
+
+
+const projects = [
+    {
+        image: g2048Img,
+        title: "2048",
+        link: "/2048"
+    },
+    {
+        image: sudokuImg,
+        title: "Sudoku",
+        link: "/sudoku"
+    },
+    {
+        image: wordleImg,
+        title: "Wordle",
+        link: "/wordle"
+    }
+];
+
+
+
+
+export default projects;
